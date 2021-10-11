d9a364e2094c9bc9c914303798222cdb215bc2fd
Sergei Afanasev <afanserg80@gmail.com>
2021-10-11 14:19:52 +0300

    update services

--- src/main/java/com/javaschool/mobile/service/AddressService.java
+++ src/main/java/com/javaschool/mobile/service/AddressService.java
@@ -1,4 +1,13 @@
 package com.javaschool.mobile.service;
 
+import com.javaschool.mobile.entity.Address;
+
+import java.util.List;
+
 public interface AddressService {
+
+    Address getAddressById(int id);
+    void saveAddress(Address address);
+    void deleteAddress(int id);
+    List<Address> getAllAddresses();
 }
